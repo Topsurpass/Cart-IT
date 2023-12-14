249ed9cb479f22610fc6500292a0d74f620da9db
Temitope Olowosuyi <Temitope.Olowosuyi@etranzact.com>
2023-12-14 12:02:50 +0100

    fix typeerror from request param

--- Flask_Mongo/api/v1/category/views.py
+++ Flask_Mongo/api/v1/category/views.py
@@ -28,8 +28,8 @@ def add_category():
     })
     return jsonify(message='New category added'),201
 
-@app_category.route('/edit/<index>', methods=['PUT'], strict_slashes=False)
-def edit_category(index: str):
+@app_category.route('/edit/<int:index>', methods=['PUT'], strict_slashes=False)
+def edit_category(index: int):
     """Edit category of a product by index"""
     name = request.form.get('name')
     description = request.form.get('description')
