5256032bc73336b291f8a2cc1c231858f64083a6
Temitope Olowosuyi <Temitope.Olowosuyi@etranzact.com>
2023-12-15 00:38:17 +0100

    added new endpoint to edit and list all product

--- Flask_Mongo/api/v1/product/views.py
+++ Flask_Mongo/api/v1/product/views.py
@@ -39,3 +39,81 @@ def add_product():
         'category_id': category_dict['_id'],
     })
     return jsonify(message='New product added'),201
+
+@app_product.route('/edit/<int:index>', methods=['PUT'], strict_slashes=False)
+def edit_product(index: int):
+    """Edit a product  of a product by index"""
+    name = request.form.get('name')
+    image_url = request.form.get('image_url')
+    description = request.form.get('description')
+    category = request.form.get('category')
+    price = request.form.get('price')
+    quantity = request.form.get('quantity')
+    session_id = request.cookies.get('session_id')
+
+    if not all(key in request.form for key in ['name', 'image_url', 'description', 'category', 'price', 'quantity']):
+        return jsonify(message='Missing required fields'), 400
+    if not session_id:
+        abort(401)
+    merchant = AUTH.get_user_from_session_id(session_id)
+    if not merchant:
+        abort(403)
+    category_dict = CATEGORY_db.find_merchant({'merchant_id': merchant['_id'], 'name': category})
+    if not category_dict:
+        abort(404, 'Category does not exist')
+    projection = {
+        'name': 1,
+        'image_url': 1,
+        'description': 1,
+        'category': 1,
+        'price': 1,
+        'quantity': 1,
+        'merchant_id': 1,
+        'category_id': 1,
+    }
+    product_list = PRODUCT_db.find_all_merchant({'merchant_id': merchant['_id']}, projection)
+    if index < 0 or index >= len(product_list):
+        abort(404)
+    selected_product = product_list[index]
+    _id_dict = selected_product.get('_id')
+    if not isinstance(_id_dict, dict) or '$oid' not in _id_dict:
+        abort(404)
+
+    _id = ObjectId(_id_dict['$oid'])
+    PRODUCT_db.update_merchant(
+        {'_id': _id},
+        {
+            'name': name,
+            'image_url': image_url,
+            'description': description,
+            'category': category,
+            'price': price,
+            'quantity': quantity,
+            'merchant_id': merchant['_id'],
+            'category_id': category_dict['_id'],
+        }
+    )
+    return jsonify(message='Product updated successfully'),200
+
+@app_product.route('/all', methods=['GET'], strict_slashes=False)
+def list_all_products():
+    """List all the Products created under a category by a user"""
+    session_id = request.cookies.get('session_id')
+    if not session_id:
+        abort(401)
+    merchant = AUTH.get_user_from_session_id(session_id)
+    if not merchant:
+        abort(403)
+    projection = {
+        'name': 1,
+        'image_url': 1,
+        'description': 1,
+        'category': 1,
+        'price': 1,
+        'quantity': 1,
+        'merchant_id': 1,
+        'category_id': 1,
+    }
+    products = PRODUCT_db.find_all_merchant({'merchant_id': merchant['_id']}, projection)
+    return jsonify(products), 200
+
