86e7d63ff50417f471b7ef42f158b01d82bbe0c1
Temz <temitopeabiodun685@gmail.com>
2023-12-14 13:37:11 +0000

    added the edit and delete category api

--- Flask_Mongo/api/v1/category/views.py
+++ Flask_Mongo/api/v1/category/views.py
@@ -44,18 +44,56 @@ def edit_category(index: int):
     if not merchant:
         abort(403)
     projection = {
-        'merchant_id': 0
+        '_id': 1,
+        'merchant_id': 1,
+        'name': 1,
+        'description': 1
     }
     category_list = CATEGORY_db.find_all_merchant({'merchant_id': merchant['_id']}, projection)
     if index < 0 or index >= len(category_list):
         abort(404)
     selected_category = category_list[index]
+    _id_dict = selected_category.get('_id')
+    if not isinstance(_id_dict, dict) or '$oid' not in _id_dict:
+        abort(404)
 
+    _id = ObjectId(_id_dict['$oid'])
+    
     CATEGORY_db.update_merchant(
-        {'_id': ObjectId(selected_category['_id'])},
+        {'_id': _id},
         {'name': name, 'description': description}
     )
     return jsonify(message='Category updated successfully'),200
+
+
+@app_category.route('/delete/<int:index>', methods=['DELETE'], strict_slashes=False)
+def delete_category(index: int):
+    """Delete a category of a product by index"""
+    session_id = request.cookies.get('session_id')
+    if not session_id:
+        abort(401)
+    merchant = AUTH.get_user_from_session_id(session_id)
+    if not merchant:
+        abort(403)
+    projection = {
+        '_id': 1,
+        'merchant_id': 1,
+        'name': 1,
+        'description': 1
+    }
+
+    category_list = CATEGORY_db.find_all_merchant({'merchant_id': merchant['_id']}, projection)
+    if index < 0 or index >= len(category_list):
+        abort(404)
+    selected_category = category_list[index]
+    _id_dict = selected_category.get('_id')
+    if not isinstance(_id_dict, dict) or '$oid' not in _id_dict:
+        abort(404)
+
+    _id = ObjectId(_id_dict['$oid'])
+    
+    CATEGORY_db.delete_merchant({'_id': _id})
+    return jsonify(message='Category deleted successfully'),200
 
 
 @app_category.route('/all', methods=['GET'], strict_slashes=False)
