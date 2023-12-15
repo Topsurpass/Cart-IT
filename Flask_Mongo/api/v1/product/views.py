95d270ce954ffa695477b3ceb0303f79224d450a
Temz <temitopeabiodun685@gmail.com>
2023-12-15 00:44:53 +0000

    added changes

--- Flask_Mongo/api/v1/product/views.py
+++ Flask_Mongo/api/v1/product/views.py
@@ -18,7 +18,7 @@ def add_product():
     quantity = request.form.get('quantity')
     session_id = request.cookies.get('session_id')
 
-    if not all(key in request.form for key in ['name', 'title', 'image_url', 'description', 'category', 'price', 'quantity']):
+    if not all(key in request.form for key in ['name', 'image_url', 'description', 'category', 'price', 'quantity']):
         return jsonify(message='Missing required fields'), 400
     if not session_id:
         abort(401)
